9ba9b77f99bd277795384b61011420c04beb241e
henrix <henni19790@googlemail.com>
2016-07-26 15:36:40 +0000

    Refactorings. Switched to JACK instead of self calculated sine for testing.

--- code/DSP_Synth/DSP_Synth.pro
+++ code/DSP_Synth/DSP_Synth.pro
@@ -11,21 +11,22 @@ greaterThan(QT_MAJOR_VERSION, 4): QT += widgets printsupport
 TARGET = DSP_Synth
 TEMPLATE = app
 
-DEFINES += QCUSTOMPLOT_USE_LIBRARY
-
 SOURCES += main.cpp\
     display.cpp \
     jackclient.cpp \
-    #qcustomplot/qcustomplot.cpp
+    ../qcustomplot/qcustomplot.cpp
 
 HEADERS  += display.hpp \
     jackclient.hpp \
-    #qcustomplot/qcustomplot.h
+    ../qcustomplot/qcustomplot.h
 
 #INCLUDEPATH += /opt/ti-processor-sdk-linux-am57xx-evm-02.00.02.11/targetNFS/usr/share/ti/cgt-c6x/include \
 #    /opt/ti-processor-sdk-linux-am57xx-evm-02.00.02.11/targetNFS/usr/share/ti/opencl \
 #    /opt/ti-processor-sdk-linux-am57xx-evm-02.00.02.11/targetNFS/usr/include
 
-LIBS += -ljack -laudio-x15 -lOpenCL -locl_util #-l$$QCPLIB
+LIBS += -ljack ../lib-shared/libaudio-x15.so -lboost_iostreams -lboost_system -lboost_filesystem #-lOpenCL -locl_util #-l$$QCPLIB
 
 FORMS += mainwindow.ui
+
+CONFIG += c++11
+
--- code/DSP_Synth/main.cpp
+++ code/DSP_Synth/main.cpp
@@ -18,40 +18,34 @@
  ***********************************************************************/
 
 #define	PAD	0
+#define GNUPLOT_DEPRECATE_WARN
 
-//#define __CL_ENABLE_EXCEPTIONS
-//#include <CL/cl.hpp>
-#include "display.hpp"
 #include "jackclient.hpp"
-#include "../lib/AudioAPI.hpp"
-#include "../lib/CallbackResponse.hpp"
+#include "../lib-shared/API.hpp"
+#include "gnuplot-iostream.h"
 #include <QApplication>
 #include <jack/jack.h>
 #include <math.h>
 #include <iostream>
 #include <fstream>
-#include "../lib/WaveIO.hpp"
+
+void callback(CallbackResponse *clbkRes);
+void callbackJACK(jack_nframes_t n_frames, jack_default_audio_sample_t *in, jack_default_audio_sample_t *out);
 
 int N = (16*1024);
 bool fftFinished = false, ifftFinished = false;
-
-void callbackFFT(CallbackResponse *clbkRes);
-void callbackIFFT(CallbackResponse *clbkRes);
-void callbackJACK(jack_nframes_t n_frames, jack_default_audio_sample_t *in, jack_default_audio_sample_t *out);
+float *y;
+//Gnuplot gp;
+API api(callback);
+float *xFFT, *xIFFT;
 
 int main(int argc, char* argv[])
 {
-    AudioAPI *api;
-    int sampling_rate = 48000;
-    int bufsize = 2*N;
-    //float *x = (float*) __malloc_ddr(sizeof(float)*2*N);
-    //float *y = (float*) __malloc_ddr(sizeof(float)*2*N);
-    float *x = new float[2*N];
-    float *y = new float[2*N];
+    api.prepareFFT(N);
+    //gp << "set yrange [-1:1]\n";
 
-    //QApplication a(argc, argv);
-    //Display w;
-    //w.show();
+    xFFT = new float[2*N];
+    xIFFT = new float[2*N];
 
     /**
      * Generate sine
@@ -59,9 +53,9 @@ int main(int argc, char* argv[])
     std::ofstream sinout("../../test/data/sine.txt");
     if (sinout.is_open()){
         for (int i=0; i < N; i++){
-            x[PAD + 2*i] = sin(2*M_PI*64*i / (double) N);
-            x[PAD + 2*i + 1] = 0;
-            sinout << x[PAD + 2*i] << std::endl;
+            xFFT[PAD + 2*i] = sin(2*M_PI*64*i / (double) N);
+            xFFT[PAD + 2*i + 1] = 0;
+            sinout << xFFT[PAD + 2*i] << std::endl;
         }
         sinout.close();
     }
@@ -71,34 +65,36 @@ int main(int argc, char* argv[])
 
     //JACKClient jackClient;
     //jackClient.addCallback(callbackJACK);
-    api = new AudioAPI();
-    api->ocl_DSPF_sp_fftSPxSP(N, x, y, 4, 16384, callbackFFT);
+    api.prepareFFT(N);
+    api.prepareIFFT(N); 
+
+    api.ocl_DSPF_sp_fftSPxSP(xFFT, 4, N);   
+
     while(!fftFinished){}
     for (int i=0; i < 2*N; i++){
-        x[i] = y[i];
+        xIFFT[i] = y[i];
     }
-
-    api->ocl_DSPF_sp_ifftSPxSP(N, x, y, 4, 16384, callbackIFFT);
-
+    api.ocl_DSPF_sp_ifftSPxSP(xIFFT, 4, N);
     while(!ifftFinished){}
-    std::cout << "Main finished" << std::endl;
 
-    delete api;
-    delete[] x;
-    delete[] y;
-    //__free_ddr(x);
-    //__free_ddr(y);
+    delete[] xFFT;
+    delete[] xIFFT;
 
-    //return a.exec(); //TODO: Create JACKClient in new thread (fix QT return a.exec()); Move memory allocation to AudioAPI;
+    return 0;
 }
 
 /**
  * Callbacks
  */
-void callbackFFT(CallbackResponse *clbkRes){
+void callback(CallbackResponse *clbkRes){
     //std::cout << "Event type callback FFT: " << type << std::endl;
-    //CallbackResponse *clbkRes = (CallbackResponse*) user_data;
-    float *y = clbkRes->getDataPtr();
+    y = clbkRes->getDataPtr();
+    //int n = clbkRes->getN();
+    /*std::vector<float> pts(n);
+    pts.assign(_y, _y+n);
+    for (int i=0; i < pts.size(); i++){
+        std::cout << i << ": " << pts[PAD + 2*i + 1] << std::endl;
+    }*/
 
     if (clbkRes->getOp() == CallbackResponse::FFT){
         std::ofstream fftoutsine("../../test/data/fft_sine.txt");
@@ -112,17 +108,15 @@ void callbackFFT(CallbackResponse *clbkRes){
             std::cout << "Couldn't write FFT sine test output" << std::endl;
         }
         std::cout << "FFT calculation completed." << std::endl;
+
+        /*gp << "plot '-' binary" << gp.binFmt1d(pts, "array") << "with lines notitle\n";
+        gp.sendBinary1d(pts);
+		gp.flush();*/
+
         delete clbkRes;
         fftFinished = true;
     }
-}
-
-void callbackIFFT(CallbackResponse *clbkRes){
-    //std::cout << "Event type callback IFFT: " << type << std::endl;
-    //CallbackResponse *clbkRes = (CallbackResponse*) user_data;
-    float *y = clbkRes->getDataPtr();
-
-    if (clbkRes->getOp() == CallbackResponse::IFFT){
+    else if (clbkRes->getOp() == CallbackResponse::IFFT){
         std::ofstream ifftout("../../test/data/ifft_sine_spectrum.txt");
         if (ifftout.is_open()){
             for (int i=0; i < N; i++){
@@ -140,6 +134,18 @@ void callbackIFFT(CallbackResponse *clbkRes){
     }
 }
 
+bool flag = false;
 void callbackJACK(jack_nframes_t n_frames, jack_default_audio_sample_t *in, jack_default_audio_sample_t *out){
-    std::cout << "_process() callback called with " << n_frames << " called" << std::endl;
+    //std::cout << "_process() callback called with " << n_frames << " called" << std::endl;
+    /*std::cout << "in: " << in[n_frames - 1] << std::endl;
+    if (!flag){
+        x = new float[n_frames*2];
+        flag = true;
+    }
+
+    for (int i=0; i < n_frames; i++){
+        x[PAD + 2*i] = in[i];
+        x[PAD + 2*i + 1] = 0;
+    }*/
+    //api.ocl_DSPF_sp_fftSPxSP(x, 4, n_frames);
 }
