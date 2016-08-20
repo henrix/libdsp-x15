c07d76aee46434e0104a9cd624d74c66f431cafd
Travis CI <travis@travis-ci.org>
2016-08-20 16:02:38 +0000

    Deploy code docs to GitHub Pages Travis build: 10
    Commit: b8e7eb871a26aea5b770cd2071b30c586563c3a7

--- html/search/all_6f.js
+++ html/search/all_6f.js
@@ -2,6 +2,8 @@ var searchData=
 [
   ['ocl_5fdspf_5fsp_5ffftspxsp',['ocl_DSPF_sp_fftSPxSP',['../classAPI.html#a62e9ed0f473af9c0f38bece16204b154',1,'API']]],
   ['ocl_5fdspf_5fsp_5ffilter_5fbiquad',['ocl_DSPF_sp_filter_biquad',['../classAPI.html#a7aed755ef2f52e99f88ef915c64b12cb',1,'API']]],
+  ['ocl_5fdspf_5fsp_5ffir_5fr2',['ocl_DSPF_sp_fir_r2',['../classAPI.html#a9dafdbc1ba4a5883ea5945de6c3e4ee7',1,'API']]],
   ['ocl_5fdspf_5fsp_5fifftspxsp',['ocl_DSPF_sp_ifftSPxSP',['../classAPI.html#a8fdad78679cbaae85b52495e06097b14',1,'API']]],
+  ['ocl_5fdspf_5fsp_5fiir',['ocl_DSPF_sp_iir',['../classAPI.html#ab5ad95af49d2276b45e94dd283b464e3',1,'API']]],
   ['ops',['Ops',['../classCallbackResponse.html#afa6b8bf2e6fb63f62a5fb5f9a53bb89b',1,'CallbackResponse']]]
 ];
