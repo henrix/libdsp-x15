d238b1003a62738eebfbde78d83a21a0b6bb2e77
Travis CI <travis@travis-ci.org>
2016-08-21 12:26:50 +0000

    Deploy code docs to GitHub Pages Travis build: 17
    Commit: 72cffe5c193af1d9ce45025d523b232b0e3add0d

--- html/search/all_73.js
+++ html/search/all_73.js
@@ -1,8 +1,5 @@
 var searchData=
 [
   ['setcallback',['setCallback',['../classAPI.html#af673b62f6a0601444a6f1d4aff50046c',1,'API']]],
-  ['setdataptr',['setDataPtr',['../classCallbackResponse.html#aab9225266bb4ca103f9cfc6cb1c17299',1,'CallbackResponse']]],
-  ['setdatasize',['setDataSize',['../classCallbackResponse.html#ae4da1293f0198dc8d4c21a61073be01d',1,'CallbackResponse']]],
-  ['setdebug',['setDebug',['../classAPI.html#a5ffb7593cf14abcbebda3e2ff012e2e7',1,'API']]],
-  ['setop',['setOp',['../classCallbackResponse.html#abf2368b311942072b44912ebe80b0c1c',1,'CallbackResponse']]]
+  ['setdebug',['setDebug',['../classAPI.html#a5ffb7593cf14abcbebda3e2ff012e2e7',1,'API']]]
 ];
