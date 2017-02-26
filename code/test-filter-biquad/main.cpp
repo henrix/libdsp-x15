40f937c880557d3030f45b04797271fa543f002e
henrix <henni19790@googlemail.com>
2017-02-26 23:22:33 +0000

    optimized filter test script and updated result plots

--- code/test-filter-biquad/main.cpp
+++ code/test-filter-biquad/main.cpp
@@ -22,27 +22,42 @@
 #include <fstream>
 #include <cstring>
 #include <cmath>
+#include <algorithm>
 
 void callbackDSP(CallbackResponse *clbkRes);
+void calcTransferFunction(float xPoints[], float yPoints[], int N, API::FILTER_TYPE type, float a0, float a1, float a2, float b1, float b2);
 
 const int N_SAMPLES = 512;
 bool DspOperationFinished = false;
+
+/*
+    libdsp-x15 offers two configuration methods for DSP filter operation
+        1. Using normalized filter coefficients a0, a1, a2, b1, b2. 
+           More information can be found here: 
+           http://www.musicdsp.org/files/Audio-EQ-Cookbook.txt
+        2. Using the following user defined parameters
+            - Filter length (N)
+            - Cutoff Frequency
+            - Sampling Frequency
+            - Quality Factor
+            - Gain
+*/
 
 int main(){
     API api(callbackDSP);
 
     api.setDebug(true);
     api.prepareFILTER_BIQUAD(N_SAMPLES);
-
+    
+    /*
+        - Type: Lowpass
+        - 48 kHz sample rate
+        - 200 Hz cutoff
+        - 0.707 Q factor
+        - 6 dB gain
+    */
     float b[3];
     float a[2];
-    /*
-        Lowpass:
-            - 48 kHz sample rate
-            - 200 Hz cutoff
-            - 0.707 Q factor
-            - 6 dB gain
-    */
     b[0] = 0.0001682236792723621; //b0
     b[1] = 0.0003364473585447242; //b1
     b[2] = 0.0001682236792723621; //b2
@@ -52,22 +67,39 @@ int main(){
 
     float Fc = 10000.0;
     float Fs = 48000.0;
-    float Q = 0.707;
+    float Q = 1.0;
     float peakGain = 6.0;
-    API::FILTER_TYPE type = API::LP;
+    API::FILTER_TYPE type = API::NOTCH;
+    float xTransfer[N_SAMPLES], yTransfer[N_SAMPLES];
+    std::vector<float> coeffs = API::calcBiquadCoefficients(type, Fc, Fs, Q, peakGain);
+    calcTransferFunction(xTransfer, yTransfer, N_SAMPLES, type, coeffs.at(0), coeffs.at(1), coeffs.at(2), coeffs.at(3), coeffs.at(4));
     api.configFILTER_BIQUAD(type, Fc, Fs, Q, peakGain);
 
-    float freq = 1000.0; //Hz
+    float freq1 = 1000.0; //Hz
     float *buf_in = api.getBufIn(CallbackResponse::FILTER_BIQUAD);
-    std::ofstream sinout("../../test/data/crossover/sine.txt");
+    std::ofstream sinout1k("../../test/data/filter/sine_1kHz.txt");
+    std::ofstream transferXOut("../../test/data/filter/transferX.txt");
+    std::ofstream transferYOut("../../test/data/filter/transferY.txt");
     for (int i=0; i < N_SAMPLES; i++){
-        buf_in[i] = sin(2*M_PI*freq*i / Fs);
-        sinout << buf_in[i] << std::endl;
+        buf_in[i] = sin(2.0*M_PI*freq1*(float)i / Fs);
+        sinout1k << buf_in[i] << std::endl;
+        transferXOut << xTransfer[i] << std::endl;
+        transferYOut << yTransfer[i] << std::endl;
     }
-    sinout.close();
+    sinout1k.close();
 
     api.ocl_DSPF_sp_filter_biquad();
+    while(!DspOperationFinished){}
+    DspOperationFinished = false;
+    float freq2 = 10000.0;
+    std::ofstream sinout10k("../../test/data/filter/sine_10kHz.txt");
+    for (int i=0; i < N_SAMPLES; i++){
+        buf_in[i] = sin(2.0*M_PI*freq2*(float)i / Fs);
+        sinout10k << buf_in[i] << std::endl;
+    }
+    sinout10k.close();
 
+    api.ocl_DSPF_sp_filter_biquad();
     while(!DspOperationFinished){}
 
     return 0;
@@ -77,13 +109,34 @@ int main(){
  * Callbacks
  */
 void callbackDSP(CallbackResponse *clbkRes){
+    static int count = 0;
     float *y = clbkRes->getDataPtr();
 
-    std::ofstream filter_out("../../test/data/crossover/output.txt");
-    for (int i=0; i < N_SAMPLES; i++){
-        filter_out << y[i] << std::endl;
+    if (count == 0){ //1kHz sine
+        std::ofstream filteredSine1kHz("../../test/data/filter/filtered_sine_1kHz.txt");
+        for (int i=0; i < N_SAMPLES; i++)
+            filteredSine1kHz << y[i] << std::endl;
+        filteredSine1kHz.close();
     }
-    filter_out.close();
-
+    else { //10 kHz sine
+        std::ofstream filteredSine10kHz("../../test/data/filter/filtered_sine_10kHz.txt");
+        for (int i=0; i < N_SAMPLES; i++)
+            filteredSine10kHz << y[i] << std::endl;
+        filteredSine10kHz.close();
+    }
+    count++;
     DspOperationFinished = true;
+}
+
+void calcTransferFunction(float xPoints[], float yPoints[], int N, API::FILTER_TYPE type, float a0, float a1, float a2, float b1, float b2){
+    for (int idx=0; idx < 512; idx++){
+        double w = idx / ((double)N - 1.0) * M_PI; //lin scale
+        double phi = std::pow(sin(w/2.0), 2);
+        double y = std::log(std::pow(a0+a1+a2, 2.0) - 4.0*(a0*a1 + 4.0*a0*a2 + a1*a2)*phi + 16.0*a0*a2*phi*phi) - std::log(std::pow(1+b1+b2, 2.0) - 4.0*(b1 + 4*b2 + b1*b2)*phi + 16.0*b2*phi*phi);
+        y = y * 10.0 / std::log(10.0);
+        if (y < -200.0)
+            y = -200.0;
+        xPoints[idx] = (idx / ((double)N - 1.0) * 48000.0 / 2.0); //lin scale
+        yPoints[idx] = y;
+    }
 }
