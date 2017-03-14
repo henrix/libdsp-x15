c4460798e7cb36af9a11908558732e60bf542f90
Travis CI <travis@travis-ci.org>
2017-03-14 12:14:34 +0000

    Deploy code docs to GitHub Pages Travis build: 45
    Commit: 52f889f36f9944e8d9d9d651337678c9984daabe

--- html/search/functions_65.js
+++ html/search/functions_65.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['enqueuecontinuoustask',['enqueueContinuousTask',['../classTaskProcessor.html#a561c8541eb94bdde362ae1a8ed5bcf8e',1,'TaskProcessor']]],
   ['enqueuetask',['enqueueTask',['../classTaskProcessor.html#a974deebbccc9b99472161e981436090f',1,'TaskProcessor']]]
 ];
